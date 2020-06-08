4b956958211b488d59a4e736ecd5c043ca18cbe8
Jordan Sewell <jrsewell400@gmail.com>
2020-06-08 11:06:50 -0600

    Update foodie poro

--- app/poros/foodie_info.rb
+++ app/poros/foodie_info.rb
@@ -1,17 +1,22 @@
 class FoodieInfo
-  attr_reader :end_location, 
+  attr_reader :id,
+              :end_location, 
               :forecast,
-              :travel_time
+              :travel_time,
+              :restaurant
               
   def initialize(location, weather, distance, food)
     @weather = convert_temp(weather[:current][:temp]).round
     @summary = weather[:current][:weather][0][:description]
-
+    @restaurant_name = food[:restaurants][0][:restaurant][:name]
+    @restaurant_address = food[:restaurants][0][:restaurant][:location][:address]
+    
+    #to serialize
+    @id = nil
     @end_location = location[:results][0][:formatted_address]
     @forecast = create_forecast_hash(@weather, @summary)
     @travel_time = distance[:routes][0][:legs][0][:duration][:text]
-    @restaurant = 
-    # binding.pry
+    @restaurant = create_restaurant_hash(@restaurant_name, @restaurant_address)
   end
 
   def convert_temp(temp)
@@ -22,6 +27,13 @@ class FoodieInfo
     hashed_info = {}
     hashed_info[:summary] = summary
     hashed_info[:temperature] = weather
+    hashed_info
+  end
+
+  def create_restaurant_hash(name, address)
+    hashed_info = {}
+    hashed_info[:name] = name
+    hashed_info[:address] = address
     hashed_info
   end
 end
