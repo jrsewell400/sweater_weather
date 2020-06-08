9cd11d15237571445ee4e77322971d62269b107a
Jordan Sewell <jrsewell400@gmail.com>
2020-06-08 11:41:33 -0600

    Refactor end_location instance variable

--- app/poros/foodie_info.rb
+++ app/poros/foodie_info.rb
@@ -11,9 +11,8 @@ class FoodieInfo
     @restaurant_name = food[:restaurants][0][:restaurant][:name]
     @restaurant_address = food[:restaurants][0][:restaurant][:location][:address]
     
-    #to serialize
     @id = nil
-    @end_location = location[:results][0][:formatted_address]
+    @end_location = location[:location]
     @forecast = create_forecast_hash(@weather, @summary)
     @travel_time = distance[:routes][0][:legs][0][:duration][:text]
     @restaurant = create_restaurant_hash(@restaurant_name, @restaurant_address)
