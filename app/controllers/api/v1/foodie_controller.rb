2e6892dad57b4f8a3a08987bd1999790f163b11f
Jordan Sewell <jrsewell400@gmail.com>
2020-06-08 11:41:49 -0600

    Refactor controller and clean up

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -1,21 +1,11 @@
 class Api::V1::FoodieController < ApplicationController
   def show
-    # Getting weather information for end location
+    # If I had more time, I would refactor this to use a facade to make this controller skinnier
     location_info = GoogleService.new.get_geocode(params[:end])
-    lat = location_info[:results][0][:geometry][:location][:lat]
-    long = location_info[:results][0][:geometry][:location][:lng]
-    weather_info = WeatherService.new.weather_data(lat, long)
-
-    # Getting distance information between locations
+    weather_info = WeatherService.new.weather_data(location_info[:lat], location_info[:lng])
     distance_info = GoogleService.new.get_distance(params[:start], params[:end])
-
-    # Getting restaraunt information from Zomato
-    food_info = ZomatoService.new.get_restaraunt(lat, long, params[:search])
-
-    # Information aggregated in poro
+    food_info = ZomatoService.new.get_restaraunt(location_info[:lat], location_info[:lng], params[:search])
     foodie_info = FoodieInfo.new(location_info, weather_info, distance_info, food_info)
-
-    # Render serialized response after aggregation
     render json: FoodieSerializer.new(foodie_info)
   end
 end
