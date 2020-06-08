1363573a785890429240a39553d9f4595475575d
Jordan Sewell <jrsewell400@gmail.com>
2020-06-08 09:55:29 -0600

    Update foodie poro

--- app/poros/foodie_info.rb
+++ app/poros/foodie_info.rb
@@ -1,9 +1,24 @@
 class FoodieInfo
+  attr_reader :end_location, 
+              :forecast
+              
+
   def initialize(location, weather)
-    @location = location
-    @weather = weather
+    @end_location = location[:results][0][:formatted_address]
+    @weather = convert_temp(weather[:current][:temp]).round
+    @summary = weather[:current][:weather][0][:description]
+    @forecast = create_forecast_hash(@weather, @summary)
     binding.pry
   end
 
-  
+  def convert_temp(temp)
+    (temp - 273.15) * 1.8 + 32
+  end
+
+  def create_forecast_hash(weather, summary)
+    hashed_info = {}
+    hashed_info[:summary] = summary
+    hashed_info[:temperature] = weather
+    hashed_info
+  end
 end
