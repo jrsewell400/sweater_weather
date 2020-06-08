53ea0bc419e31e8b3b9bace5abf3f20295cd2673
Jordan Sewell <jrsewell400@gmail.com>
2020-06-07 22:21:06 -0600

    Create test for weather request api call

---
+++ spec/requests/api/v1/weather/weather_request_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'Retreive City Weather' do
+  it 'returns required information.' do
+    location = {location: "denver, co"}
+    
+    get '/api/v1/forecast', params: location
+
+    expect(response).to be_successful
+
+    forecast = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+    keys = [:id,
+            :city,
+            :state,
+            :country,
+            :time,
+            :timezone_offset,
+            :timezone,
+            :current,
+            :current_high,
+            :current_low,
+            :summary,
+            :icon,
+            :feels_like,
+            :humidity,
+            :visibility,
+            :uv_index,
+            :uv_category,
+            :sunrise,
+            :sunset,
+            :daily_forecasts,
+            :hourly_forecasts]
+    
+    expect(forecast).to be_a(Hash)
+    expect(forecast.length).to eq(21)
+    expect(forecast.keys).to eq(keys)
+    expect(forecast[:daily_forecasts].length).to eq(7)
+    expect(forecast[:daily_forecasts][0].keys).to eq([:day, :icon, :summary, :high, :low])
+    expect(forecast[:hourly_forecasts].length).to eq(8)
+    expect(forecast[:hourly_forecasts][0].keys).to eq([:time, :icon, :temp])
+  end
+end
