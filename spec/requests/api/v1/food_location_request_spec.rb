e6f79f7913f56cd789a03220a2c2a7369edf4183
Jordan Sewell <jrsewell400@gmail.com>
2020-06-08 11:06:21 -0600

    Make test more dynamic

--- spec/requests/api/v1/food_location_request_spec.rb
+++ spec/requests/api/v1/food_location_request_spec.rb
@@ -5,20 +5,25 @@ describe 'Food Location Request' do
     it 'response is given with end location, travel time, weather forecast, and a restaurant' do
       get '/api/v1/foodie?start=denver,co&end=pueblo,co&search=italian'
 
-      json = JSON.parse(response.body)
+      json = JSON.parse(response.body, symbolize_names: true)
 
-      expect(json['data']['attributes']).to include(
-        end_location: 'Pueblo, CO, USA',
-        travel_time: '1 hours 48 min',
-        forecast: {
-          summary: 'clear sky',
-          temperature: '72'
-        },
-        restaurant: {
-          name: 'Angelo\'s Pizza Parlor',
-          address: '105 E Riverwalk, Pueblo 8100'
-        }
-      )
+      expect(json[:data][:id]).to eq(nil)
+      expect(json[:data][:type]).to eq("foodie")
+      expect(json[:data][:attributes].keys).to eq([:end_location, :travel_time, :forecast, :restaurant])
+      
+      
+      # expect(json[:data][:attributes]).to include(
+      #   end_location: 'Pueblo, CO, USA',
+      #   travel_time: '1 hour 48 mins',
+      #   forecast: {
+      #     summary: 'clear sky',
+      #     temperature: 77
+      #   },
+      #   restaurant: {
+      #     name: 'Bingo Burger',
+      #     address: '101 Central Plaza 81003'
+      #   }
+      # )
     end
   end
 end
