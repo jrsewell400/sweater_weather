fd9c66ba7c7ce42e308b7d78e4c60d3c09cb831c
Jordan Sewell <jrsewell400@gmail.com>
2020-06-08 10:19:55 -0600

    Update foodie controller

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -5,7 +5,11 @@ class Api::V1::FoodieController < ApplicationController
     lat = location_info[:results][0][:geometry][:location][:lat]
     long = location_info[:results][0][:geometry][:location][:lng]
     weather_info = WeatherService.new.weather_data(lat, long)
-    binding.pry
-    foodie_info = FoodieInfo.new(location_info, weather_info)
+
+    # Getting distance information between locations
+    distance_info = GoogleService.new.get_distance(params[:start], params[:end])
+    # binding.pry
+    
+    foodie_info = FoodieInfo.new(location_info, weather_info, distance_info)
   end
 end
