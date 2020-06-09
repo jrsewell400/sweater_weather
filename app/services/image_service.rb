a9d16f9ea203b7afd537c0d2daea570f2e93567d
Jordan Sewell <jrsewell400@gmail.com>
2020-06-09 14:36:34 -0600

    Remove per_page as 1 is default number of pages returned

--- app/services/image_service.rb
+++ app/services/image_service.rb
@@ -7,7 +7,6 @@ class ImageService
     response = connection.get('/search/photos') do |add|
       add.params['client_id'] = ENV['UNSPLASH-ACCESS-KEY']
       add.params[:query] = "#{location}"
-      add.params[:per_page] = "1"
     end
     JSON.parse(response.body, symbolize_names: true)
   end
