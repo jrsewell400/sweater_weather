b81e891414ca4912c9936495360849b19aafef98
Jordan Sewell <jrsewell400@gmail.com>
2020-06-09 17:11:14 -0600

    Update controller to require user param and refactor 'render json' lines to pass thru body

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,16 +3,16 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: {status: 201, body: UsersSerializer.new(user)}
+      render json: {body: UsersSerializer.new(user)}, status: 201
     else
-      render json: {status: 400, body: user.errors.full_messages.to_sentence}
+      render json: {body: user.errors.full_messages.to_sentence}, status: 400
     end
   end
 
   private
 
   def user_params
-    params.permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
 end
